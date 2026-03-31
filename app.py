065a56addddc0045f0a55165bfd94eaaff3ea738
asadbekbek33520-create <asadbek.bek33520@gmail.com>
2026-03-31 17:31:29 +0500

    Refactor app.py with consistent formatting

--- app.py
+++ app.py
@@ -121,4 +121,5 @@ def send_to_telegram(b_id, data, title):
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
